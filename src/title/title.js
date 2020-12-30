b9de4fc9809a78435a1ed8632925e709cb1b71d7
Thiago Schreck <thiagoschreck@pop-os.localdomain>
2020-12-30 09:38:49 -0300

    title now works

--- src/title/title.js
+++ src/title/title.js
@@ -1,39 +1,106 @@
-import React from 'react';
+import React, { useState }from 'react';
 import '../index.css';
 import './title-style.css';
 
-const weatherURL = "http://api.openweathermap.org/data/2.5/weather?id=3441575&appid=0823e272568368a3ed22b2cd2a830e01&units=metric";
+const CITY = 'Montevideo,UY';
+const APIKEY = '0823e272568368a3ed22b2cd2a830e01';
+const UNITS = 'metric';
 
-// function fetchWeatherData(){
-//     fetch(weatherURL)
-//       .then(response => response.json())
-//       .then((jsonData) => {
-//         // jsonData is parsed json object received from url
-//         return "jsonData";
-//       })
-//       .catch((error) => {
-//         // handle your errors here
-//         console.error(error)
-//         return null;
-//     });
-// }
-// async function fetchWeatherData() {
-//     return await fetch(
-//         weatherURL
-//     )
-//     .then((response) => {
-//         return response.json();
-//     })
-//     .catch((error) => console.log(error));
+console.clear();
+
+// const req = new XMLHttpRequest();
+// const url = `https://api.openweathermap.org/data/2.5/weather?q=${CITY}&appid=${APIKEY}&units=${UNITS}`
+// req.open("GET", url), true;
+
+// req.onreadystatechange=(e)=>{
+//     console.log(req.responseText)
 // }
 
-let test;
-class Title extends React.Component {
-    render() {
-        return(
-            <h1 className="title">Current weather for is</h1>
-        );
+// req.send();
+
+// var getJSON = function(url, callback) {
+//     var xhr = new XMLHttpRequest();
+//     xhr.open('GET', url, true);
+//     xhr.responseType = 'json';
+//     xhr.onload = function() {
+//       var status = xhr.status;
+//       if (status === 200) {
+//         callback(null, xhr.response);
+//       } else {
+//         callback(status, xhr.response);
+//       }
+//     };
+//     xhr.send();
+// };
+
+// var req = new XMLHttpRequest();
+// req.open('GET', `https://api.openweathermap.org/data/2.5/weather?q=${CITY}&appid=${APIKEY}&units=${UNITS}`, true);
+// req.onreadystatechange = function (aEvt) {
+//   if (req.readyState === 4) {
+//      if(req.status === 200)
+//       console.log(req.responseText);
+//      else
+//       console.log("Error loading page\n");
+//   }
+// };
+// req.send(null); 
+
+// let resulta = getJSON(`https://api.openweathermap.org/data/2.5/weather?q=${CITY}&appid=${APIKEY}&units=${UNITS}`,
+// function(err, data) {
+//   if (err !== null) {
+//     alert('Something went wrong: ' + err);
+//   } else {
+//     console.log('Data ' + JSON.stringify(data));
+//     return JSON.stringify(data);
+//   }
+// });
+
+// function getJSONData(){
+//     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${CITY}&appid=${APIKEY}&units=${UNITS}`)
+//         .then(response => response.json())
+//         .then(data => {return(data.name)});
+//     }
+
+// class Title extends React.Component {
+//     render() {
+//         return(
+//             <h1 className="title">Current weather for {getJSONData()} is</h1>
+//         );
+//     }
+// };
+
+// export default Title;
+
+
+// function getJSONData(){
+//     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${CITY}&appid=${APIKEY}&units=${UNITS}`)
+//         .then(response => response.json())
+//         .then(data => {return(data.name)});
+//     }
+
+// class Title extends React.Component {
+//     state = {data: {}}
+//     componentDidMount(){
+//       getJSONData();
+//     }
+//     render() {
+//         return(
+//             <h1 className="title">Current weather for {JSON.stringify(this.state.data)} is</h1>
+//         );
+//     }
+// };
+
+export default class Title extends React.Component {
+    state = {}
+  
+    componentDidMount() {
+      return fetch(`https://api.openweathermap.org/data/2.5/weather?q=${CITY}&appid=${APIKEY}&units=${UNITS}`)
+        .then((response) => response.json())
+        .then((data) => this.setState({ data }));
     }
-};
-
-export default Title;
+  
+    render() {
+      const { data } = this.state
+      return data ? <h1 className="title">Current weather for {data.name} is</h1> : "Loading...";
+    }
+  }
